71b989c69eb8e1c5c9b8c2cf97e63d42770294ee
Torsten <98063103+Torsten-GMIT@users.noreply.github.com>
2022-02-03 14:24:59 +0000

    working along lecture

---
+++ week03/firstVariables.py
@@ -0,0 +1,37 @@
+# working along with lecture week02
+# messing around with variable types
+#author: Torsten Kindt
+
+age = 32
+print (type(age))
+
+age = True
+print (type(age))
+
+age = 'hello'
+print (type(age))
+
+age = "hello"
+print (type(age))
+
+age = []
+print (type(age))
+
+age = ()
+print (type(age))
+
+age = {}
+print (type(age))
+
+ageOfPatient = {}
+age = 3
+print (type(ageOfPatient))
+print (type(age))
+
+#convert str to int and int to str
+
+print ("you are " + str(age))
+
+nextYear = int(age) + 1
+print ("next year you will be " + str(nextYear))
+
