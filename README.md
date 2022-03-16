9e760324bb7e6524a43f4fe94f9f3226f8ff7b59
Torsten <98063103+Torsten-GMIT@users.noreply.github.com>
2022-03-16 06:58:09 +0000

    additions to readme

--- README.md
+++ README.md
@@ -17,10 +17,18 @@ Lecture 2.2.1, lab 2.2
     - hello2.py
     - multiply.py
     - addOne.py
+    - readName.py
 
 Week 03
 Lecture 3.1., lab 3.1.x
     - firstVariables.py
+    - lab3.1.1-testTypes.py
+    - lab3.1.2-sub.py
+    - lab3.1.3-div.py
+    - lab3.1.4-randomGenerator.py
+    - lab3.1.5-randomFruit.py
+    - lab3.1.5.1-randomFruit2.py
+    - lab3.1.6-extra.py
 
 Lecture 3.2., lab 3.2.x
     - funWithNumbersTut.py
--- week03/funWithNumbersTut.py
+++ week03/funWithNumbersTut.py
@@ -2,13 +2,14 @@
 # working along with lecture 3.2
 # learning about some arithmetic in python
 
-# a runthrew of the labs 3.2.x
+# a runthrough of the labs 3.2.x
 
 # on strings: can be put in single or double quotes
 # a single quote in a single quote string will lead to syntax error
 # must tell python that the single quote in the str is correct
 
 # 2 ways - put string in double quotes, leave single quote as is
+
 
 name = "Torsten's father"
 print(name)
