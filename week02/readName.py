1696a845c50374788997b50574f2bdec02537ab1
Torsten <98063103+Torsten-GMIT@users.noreply.github.com>
2022-03-16 06:42:41 +0000

    workalong lecture 2.2.1

--- week02/readName.py
+++ week02/readName.py
@@ -6,6 +6,9 @@
 name = input("Enter your name: ")
 print("Hello " + name)
 
+# read in an age and print it out
+# .format 
+
 age = input("What is your age?: ")
 print("Your age is {}".format(age))
 
